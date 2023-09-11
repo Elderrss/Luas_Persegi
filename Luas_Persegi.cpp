a0f20cc4086d08350b21290a0d636324fa42ff38
Elderrss <144640867+Elderrss@users.noreply.github.com>
2023-09-11 09:33:04 +0700

    Create Luas_Persegi.cpp

---
+++ Luas_Persegi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main( ) {
+int l,s;
+cout<<"PEMROGRAMAN MENGHITUNG LUAS PERSEGI \n";
+cout<<"masukan panjang sisi persegi: ";
+cin>>s;
+l=s*s;
+cout<<"luas persegi adalah: "<<l<< endl;
+return 0;
+}
